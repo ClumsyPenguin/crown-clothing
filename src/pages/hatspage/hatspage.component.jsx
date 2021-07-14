3a9f1303b806bbd30afa1d19669e35c4e27da0e3
Seppe_Geerinckx <seppe318@gmail.com>
2021-07-14 21:49:18 +0200

    added react-router

---
+++ src/pages/hatspage/hatspage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './hatspage.styles.scss'
+
+import Directory from '../../components/directory/directory.component';
+
+const HatsPage = () => (
+  <div>
+    HATS
+  </div>
+);
+
+export default HatsPage;
