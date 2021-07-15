a87edaecc3983dcc96d9a7227399cd6faf58ccf0
Seppe_Geerinckx <seppe318@gmail.com>
2021-07-15 14:40:54 +0200

    styling sign in component

--- src/App.js
+++ src/App.js
@@ -6,18 +6,40 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
 import Header from './components/header/header.component';
 import SignInPage from './pages/signinpage/signinpage.component';
+import {auth } from './firebase/firebase.utils'
+class App extends React.Component{
+  constructor(){
+    super();
+    
+    this.state = {
+      currentUser: null
+    }
+  }
 
-function App() {
-  return (
+  unsubscribeFromAuth = null
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+    } );
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return(
     <div>
-      <Header/>
+      <Header currentUser={this.state.currentUser} />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
         <Route path="/login" component={SignInPage}/>
       </Switch>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo} from '../../assets/crown.svg'
 import './header.styles.scss'
 
-const Header = () => (
+import {auth} from '../../firebase/firebase.utils.js';
+
+const Header = ({currentUser}) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -16,9 +18,14 @@ const Header = () => (
       <Link className="option" to="/contact">
         CONTACT
       </Link>
-      <Link className="option" to="/login">
-        LOG IN
-      </Link>
+      {
+        currentUser ?
+        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+        :
+        <Link className="option" to="/login">
+          SIGN IN
+        </Link>
+      }    
     </div>
   </div>
 );
